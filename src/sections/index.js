7449ec02528d951d9ee348f5a0f79b54a0232313
Hoang <jasonkindo1710@gmail.com>
2022-10-26 09:49:08 +0700

    first

---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+import Contact from './Contact';
+import Footer from './Footer';
+import Gallery from './Gallery';
+import LandingPage from './LandingPage';
+import Navbar from './Navbar';
+import Sponsor from './Sponsor';
+import Team from './Team';
+
+export {
+   Contact, Footer, Gallery, LandingPage, Navbar, Sponsor, Team
+}
